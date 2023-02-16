a5875dfad77166a8aebc502a1bbb22ae3859e4bb
monteirosamuel74 <ss-monteiro@outlook.com>
2023-02-15 21:31:41 -0400

    Curso 58% completo

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 59
+console.log(`Velocidade total é de ${vel}`)
+if(vel>60){
+    console.log(`Velocidade acima do permitido. Multa enviada.`);
+}else{
+    console.log(`Velocidade dentro do permitido.`);
+}
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo no ${país}.`);
+if (país != 'Brasil') {
+    console.log('Você é estrangeiro.');
+}else{
+    console.log('Você é brasileiro.');
+}
